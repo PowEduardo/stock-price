335747f45c7a685e65c068609a7d1085e45fe06d
Eduardo <eduardo.a3@icloud.com>
2025-01-12 15:24:07 -0300

    Refatoracao

--- src/main/java/br/com/powtec/finance/batch/stock_price/StockPriceApplication.java
+++ src/main/java/br/com/powtec/finance/batch/stock_price/StockPriceApplication.java
@@ -10,7 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableBatchProcessing
 @ComponentScan(basePackages = {"br.com.powtec.finance"})
-@EnableJpaRepositories(basePackages = {"br.com.powtec.finance"})
+@EnableJpaRepositories(
+    basePackages = "br.com.powtec.finance.database.library.repository"
+)
 @EntityScan(basePackages = {"br.com.powtec.finance.database.library.model"})
 public class StockPriceApplication {
 
